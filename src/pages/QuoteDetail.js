4df0f22820c6262315c7c7f50eb0dc069c764e73
Joey <87529468+cloudyruer@users.noreply.github.com>
2021-12-17 01:25:29 +0800

    router added

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,22 @@
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const { quoteId } = useParams();
+
+  return (
+    <>
+      <h1>Quote Detail Page</h1>
+      <p>{quoteId}</p>
+      <Route path="/quotes/:quoteId/comments">
+        <Comments />
+      </Route>
+      {/* <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route> */}
+    </>
+  );
+};
+
+export default QuoteDetail;
